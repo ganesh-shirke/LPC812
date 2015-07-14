4c8747bbeb1e405e881f48248c3fe71d281e319a
unknown <super-saiyan-god@users.noreply.github.com>
2015-07-14 23:34:38 +0530

    Adding LPC Expresso Workspace template for developing assembly codes for LPC812

---
+++ LPC812_ASSEMBLY_CODES/LPC812_ASSEMBLY/Debug/src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../src/main.s 
+
+OBJS += \
+./src/main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU Assembler'
+	arm-none-eabi-gcc -c -x assembler-with-cpp -DDEBUG -D__CODE_RED -DCORE_M0PLUS -D__LPC8XX__ -D__REDLIB__ -g3 -mcpu=cortex-m0 -mthumb -specs=redlib.specs -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
